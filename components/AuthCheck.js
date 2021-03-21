a71e4d2c1f498920503236b976f02b85b7d76259
priyam1103 <priyampoddar89@gmail.com>
2021-03-21 23:35:53 +0530

    loading

--- components/AuthCheck.js
+++ components/AuthCheck.js
@@ -28,6 +28,8 @@ export default function AuthCheck() {
             setLoading(false)
             alert("error");
           });
+      } else {
+        setLoading(false)
       }
     }
     authCheck();
